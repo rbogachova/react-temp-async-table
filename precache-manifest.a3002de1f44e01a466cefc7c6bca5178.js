2191f9f11ae933955f74c1da5fa808b7f8fc906d
Rita Bogachov <rbogachov@usatech.com>
2020-06-11 20:28:35 -0700

    Updates

---
+++ precache-manifest.a3002de1f44e01a466cefc7c6bca5178.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b11c9e60b6146e3b8f911cf25dc6c3",
+    "url": "/react-temp-async-table/index.html"
+  },
+  {
+    "revision": "a44e874ddef17bd2066c",
+    "url": "/react-temp-async-table/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a44e874ddef17bd2066c",
+    "url": "/react-temp-async-table/static/js/2.27f4d5b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-temp-async-table/static/js/2.27f4d5b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b297e0b2c47a0c27cf86",
+    "url": "/react-temp-async-table/static/js/main.9350973c.chunk.js"
+  },
+  {
+    "revision": "0e86b86539ae13ae2780",
+    "url": "/react-temp-async-table/static/js/runtime-main.612a9f3a.js"
+  }
+]);
